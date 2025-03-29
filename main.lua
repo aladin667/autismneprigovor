f403ff728362553d1c8d5bc6f5450e71ffbbac96
aladin667 <pu55y.k1ller.42@gmail.com>
2025-03-29 23:11:57 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -872,36 +872,6 @@ createCheckbox(section1Content, "FPS Boost", false, function(value)
             workspace.Terrain.Clouds.Enabled = false
         end
 
-        local workspace = game:GetService("Workspace")
-        local leaves = {"Fir3_Leaves", "Birch1_Leaves", "Elm1_Leaves", "Palm1_Leaves"}
-
-        local oldNamecall
-        oldNamecall = hookmetamethod(game, "__namecall", function(self, ...)
-            local method = getnamecallmethod()
-            if self == workspace and method == "DescendantAdded" then
-                return oldNamecall(self, ...) 
-            end
-            return oldNamecall(self, ...)
-        end)
-
-        local function spoof(obj)
-            if table.find(leaves, obj.Name) then
-                obj.Transparency = 1
-                obj.CanCollide = false
-                if obj:IsA("BasePart") then
-                    obj.CastShadow = false
-                end
-            end
-        end
-
-        for _, obj in pairs(workspace:GetDescendants()) do
-            spoof(obj)
-        end
-
-        workspace.DescendantAdded:Connect(function(obj)
-            spoof(obj)
-        end)
-    else
         if workspace.Terrain:FindFirstChild("Clouds") then
             workspace.Terrain.Clouds.Enabled = true
         end
